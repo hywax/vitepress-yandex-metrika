caaf729d5774dbff98add408109f735ba520c520
hywax <a.hywax@gmail.com>
2024-01-09 20:04:55 +0500

    chore: yandex metrika types

---
+++ types/yandex-metrika.d.ts
@@ -0,0 +1,22 @@
+/**
+ * A minimal description of the interfaces.
+ * To learn more see - https://github.com/yandex/metrica-tag
+ */
+
+export {}
+
+interface Counter {
+  hit(): void
+}
+
+interface Ya {
+  _metrika: {
+    counters: Record<string, Counter>
+  }
+}
+
+declare global {
+  interface Window {
+    Ya?: Ya
+  }
+}
