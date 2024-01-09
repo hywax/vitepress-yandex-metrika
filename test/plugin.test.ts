3a6259dac957e6172ef8912e5fa80fd1e10578d9
hywax <a.hywax@gmail.com>
2024-01-09 20:21:07 +0500

    test: yandexMetrikaPlugin

---
+++ test/plugin.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest'
+
+describe('yandexMetrikaPlugin', () => {
+  it.todo('should hiding in dev mode', () => {
+
+  })
+
+  it.todo('should appear at the bottom of the page', () => {
+
+  })
+})
