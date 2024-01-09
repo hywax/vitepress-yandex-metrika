ca4771d116518ee976cb10b0d23d43fc0a68cd31
hywax <a.hywax@gmail.com>
2024-01-09 20:20:55 +0500

    test: yandexMetrikaHit

---
+++ test/hit.test.ts
@@ -0,0 +1,15 @@
+import { describe, it } from 'vitest'
+
+describe('yandexMetrikaHit', () => {
+  it.todo('should without window there is no error', () => {
+
+  })
+
+  it.todo('should without Ya there is no error', () => {
+
+  })
+
+  it.todo('should after transition the hit call', () => {
+
+  })
+})
